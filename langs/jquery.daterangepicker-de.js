3ed3bd388a4da21eea9a1479a67b5e1f77f8eb28
David Hainzl <david.hainzl@seekda.com>
2014-07-07 15:25:23 +0200

    Added german translation

---
+++ langs/jquery.daterangepicker-de.js
@@ -0,0 +1,42 @@
+// German translation of daterangepicker.js (0.0.5)
+// version : 0.0.1
+// author : David Hainzl
+// last updated at: 2014-07-07
+// license : MIT
+
+;(function($)
+{
+	$.dateRangePickerLanguages = $.dateRangePickerLanguages || {};
+	$.dateRangePickerLanguages ['de'] = {
+		'selected': 'Ausgew&auml;hlt:',
+		'day': 'Tag',
+		'days': 'Tage',
+		'apply': 'Schlie&szlig;en',
+		'week-1' : 'MO',
+		'week-2' : 'DI',
+		'week-3' : 'MI',
+		'week-4' : 'DO',
+		'week-5' : 'FR',
+		'week-6' : 'SA',
+		'week-7' : 'SO',
+		'month-name': [ 'JANUAR', 'FEBRUAR', 'M&Auml;RZ', 'APRIL', 'MAI', 'JUNI', 'JULI', 'AUGUST', 'SEPTEMBER', 'OKTOBER', 'NOVEMBER', 'DEZEMBER' ],
+		'shortcuts' : 'Schnellauswahl',
+		'past': 'Vorigen',
+		'following':'Nächste',
+		'previous' : 'Vorige',
+		'prev-week' : 'Woche',
+		'prev-month' : 'Monat',
+		'prev-year' : 'Jahr',
+		'next':'N&auml;chste',
+		'next-week':'Woche',
+		'next-month':'Monat',
+		'next-year':'Jahr',
+		'less-than' : 'Bitte w&auml;hlen Sie nicht mehr als %d Tage aus!',
+		'more-than' : 'Bitte w&auml;hlen Sie nicht weniger als %d Tage aus!',
+		'default-more' : 'Bitte w&auml;hlen Sie mehr als %d Tage aus!',
+		'default-single' : 'Bitte w&auml;hlen Sie ein Datum aus',
+		'default-less' : 'Bitte w&auml;hlen Sie weniger als %d Tage aus!',
+		'default-range' : 'Bitte w&auml;hlen Sie zwischen %d und %d Tagen aus!',
+		'default-default': 'Bitte wählen Sie mehrere Tage aus!'
+	};
+}(jQuery));
